0906168ae37956d47a5112e9cc752209f654a311
RatnakiranKankipati <kiranratna18@gmail.com>
2025-09-19 11:45:03 +0530

    update env file ports

--- index.ts
+++ index.ts
@@ -4,25 +4,26 @@ dotenv.config();
 import express from 'express';
 import ForgeManagerRoutes from './ForgeManagerRoutes';
 
+
 const app = express();
 app.use(express.json());
 app.use('/forge', ForgeManagerRoutes);
 
-app.listen(3000, () => {
-  const url=startNgrok(3000);
-  console.log('Server running on port 3000',url);
+app.listen(process.env.FORGE_HOSIT, () => {
+  // const url=startNgrok(3000);
+  console.log('Server running on port',process.env.FORGE_HOSIT);
 });
 
-import ngrok from 'ngrok';
-import { mycallbackurlmehtod } from './ForgeManager';
+// import ngrok from 'ngrok';
+// import { mycallbackurlmehtod } from './ForgeManager';
 
-export async function startNgrok(port: 3000): Promise<string> {
-  const url = await ngrok.connect({
-    addr: port,
-    authtoken: process.env.NGROK_AUTHTOKEN, // or hardcode for testing
-  });
+// export async function startNgrok(port: 3000): Promise<string> {
+//   const url = await ngrok.connect({
+//     addr: port,
+//     authtoken: process.env.NGROK_AUTHTOKEN, // or hardcode for testing
+//   });
 
-  console.log(`Ngrok tunnel running at: ${url}`);
-  mycallbackurlmehtod(url)
-  return url;
-}
+//   console.log(`Ngrok tunnel running at: ${url}`);
+//   mycallbackurlmehtod(url)
+//   return url;
+// }
